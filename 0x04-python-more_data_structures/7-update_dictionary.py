63105eb2d45cc34c3d52882833101f2bc00705a4
olueesuola@gmail.com <ktech2016>
2022-12-07 14:39:19 -0800

    update on task 7 and 9

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return (a_dictionary)[D[D[C[C
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return (my_dict)
